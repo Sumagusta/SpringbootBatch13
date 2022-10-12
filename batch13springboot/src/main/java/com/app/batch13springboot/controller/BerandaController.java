90ae9d2a0021d2b4bc8c1a2df0d77a6375fca92f
ryorangga.suma@gmail.com <ryorangga.suma@gmail.com>
2022-10-12 09:56:37 +0700

    update controller

--- batch13springboot/src/main/java/com/app/batch13springboot/controller/BerandaController.java
+++ batch13springboot/src/main/java/com/app/batch13springboot/controller/BerandaController.java
@@ -2,9 +2,11 @@ package com.app.batch13springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.batch13springboot.model.Mahasiswa;
 import com.app.batch13springboot.service.IMahasiswaService;
 
 @RestController 
@@ -22,11 +24,9 @@ public class BerandaController {
 	// API Req : Post, Put, Get, Delete
 	
 	@PostMapping("/post")
-	public String post() {
+	public Mahasiswa post(@RequestBody Mahasiswa mahasiswa) {
 		
-		
-		
-		return null;
+		return mahasiswaService.insert(mahasiswa);
 	}
 	
 }
